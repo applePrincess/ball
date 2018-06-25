e161a4990f5118cba41482d8942ea4cae8e9c4fd
Apple Princess <appleprincess@appleprincess.io>
2018-06-25 09:06:25 +0100

    Dependency Yampa is added.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,33 @@
+{-# LANGUAGE Arrows #-}
 module Main where
 
+import FRP.Yampa
+
+data Input
+data Maze
+
 main :: IO ()
-main = undefined
+main = do
+  _ <- reactimate initialize sense actuate process
+  return ()
+
+
+initialize :: IO [Input]
+initialize = return []
+
+sense :: Bool -> IO (DTime, Maybe [Input])
+sense b = undefined
+
+actuate :: Bool -> Maze -> IO Bool
+actuate b maze = undefined
+
+process :: SF [Input] Maze
+process = undefined
+
+{-
+eactimate :: IO a                          -- init
+           -> (Bool -> IO (DTime, Maybe a)) -- input/sense
+           -> (Bool -> b -> IO Bool)        -- output/actuate
+           -> SF a b                        -- process/signal function
+           -> IO ()
+-}
